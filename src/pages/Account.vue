3ca62b249cf7cec5fd2e9512a759e1b0ffccff6a
electric-mike <66960109+electric-mike@users.noreply.github.com>
2024-07-07 20:03:49 -0400

    Account styles

--- src/pages/Account.vue
+++ src/pages/Account.vue
@@ -1,19 +1,44 @@
 <template>
   <div class="top account-page">
     <div v-if="userStore?.user?.id">
-      <h2>Account</h2>
-      <h3 class="account-header">{{ userStore?.user?.email }}</h3>
-      <!-- <h4 class="account-header">{{ userStore?.user?.id }}</h4> -->
-      <div class="logout">
-        <p v-if="userStore.loggingOut">
-          <i class="pi pi-sign-out" style="font-size: 0.9rem; margin-right: 0.5rem"></i> Logging out
-        </p>
-        <p @click="userStore.signOutUser" v-else>
-          <i class="pi pi-sign-out" style="font-size: 0.9rem; margin-right: 0.5rem"></i> Logout
-        </p>
+      <div class="account-top">
+        <div>
+          <h2>Account</h2>
+          <div class="info-status">
+            <h4 class="account-header">{{ userStore?.user?.email }}</h4>
+            <div class="pill">{{ userStore?.user?.role }}</div>
+          </div>
+          <p>
+            Last login:
+            {{ new Date(userStore?.user?.last_sign_in_at).toLocaleDateString('en-US') }} at
+            {{
+              new Date(userStore?.user?.last_sign_in_at).toLocaleTimeString('en-US', {
+                hour: 'numeric',
+                minute: 'numeric',
+                hour12: true,
+              })
+            }}
+          </p>
+        </div>
+        <div>
+          <!-- <h4 class="account-header">{{ userStore?.user?.id }}</h4> -->
+          <div class="logout">
+            <p v-if="userStore.loggingOut">
+              <i class="pi pi-sign-out" style="font-size: 0.9rem; margin-right: 0.5rem"></i> Logging
+              out
+            </p>
+            <p @click="userStore.signOutUser" v-else>
+              <i class="pi pi-sign-out" style="font-size: 0.9rem; margin-right: 0.5rem"></i> Logout
+            </p>
+          </div>
+        </div>
       </div>
-      <h3 class="account-conversations-header">All Conversations</h3>
       <div class="account-conversations" v-if="conversationsStore?.conversations?.length > 0">
+        <h3 class="account-conversations-header">
+          Your {{ conversationsStore?.conversations?.length }} Conversation{{
+            conversationsStore?.conversations?.length !== 1 ? 's' : ''
+          }}
+        </h3>
         <div v-for="conversation in conversationsStore?.conversations" class="account-conversation">
           <div class="account-conversation-item-header">
             <h4>{{ conversation.messages.length }} messages</h4>
