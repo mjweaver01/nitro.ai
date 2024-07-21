418d4c54b31f78734574ef6e8618eb0bf76e58d4
electric-mike <66960109+electric-mike@users.noreply.github.com>
2024-07-20 21:23:53 -0400

    bug

--- src/components/MobileNav.vue
+++ src/components/MobileNav.vue
@@ -30,7 +30,7 @@ export default {
     this.$watch(
       () => this.$route.path,
       () => {
-        sidebarSection.setForceShow(false)
+        sidebarStore.setForceShow(false)
       },
     )
   },
